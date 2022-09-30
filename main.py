29b9b3799f5adb168dfc83d019a039678df27a74
Jeremy Stookey <mrjeremystookey@gmail.com>
2022-09-30 17:43:31 -0500

    initial commit

--- main.py
+++ main.py
@@ -19,25 +19,45 @@ loginData = {
     "password": f"{password}"
 }
 
-hostname = 'https://192.168.50.1'
-login_url = '/api/login'
+hostname = 'https://192.168.50.1/api/'
+login_url = 'login'
+connection_status_url = 'status.wan.connection'
+create_client_url = 'auth.client'
+
+s = requests.sessions.session()
 
 
 def login():
     print(f'\nattempting to login to local Pepwave router at {hostname + login_url}\n')
     print(f'login data: {loginData}\n')
     urllib3.disable_warnings()
-    s = requests.session()
     resp = s.post(url=hostname + login_url,
                   headers=headers,
                   data=json.dumps(loginData),
-                  json=loginData,
                   verify=False)
     print(f"returned json: {resp.json()}\n")
     if resp.status_code != 200:
         print('error: ' + str(resp.status_code))
     else:
-        print('Success')
+        print('Success. Logged in')
+        check_status()
+
+
+def create_client():
+    print("creating new client for access token")
+    resp = s.post(url=hostname + create_client_url,
+                  headers=headers,
+                  verify=False)
+    print(f'\n{resp.json()}\n')
+    print(f'\n{resp.json()["token"]}\n')
+
+
+def check_status():
+    print("\nchecking WAN connection status")
+    resp = s.get(url=hostname + connection_status_url,
+                 headers=headers,
+                 verify=False)
+    print(f'\n{resp.json()}\n')
 
 
 if __name__ == '__main__':
